0dd692cf6e5e39b5fcda7f508ea8983d2d416e01
Muhammet Rasit Goktas <103164878+mgoktas@users.noreply.github.com>
2023-12-13 12:45:39 -0800

    Update GoogleLogin.tsx

--- screens/Auth/GoogleLogin.tsx
+++ screens/Auth/GoogleLogin.tsx
@@ -14,7 +14,7 @@ export const GoogleLogin = () => {
 
   // GoogleSignin.configure({
   //   scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-  //   webClientId: '520113159678-e8vppgdiq2d0l3sfjesg3e8jpvmqhc09.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+  //   webClientId: '<YOUR-CLIENT-ID>', // client ID of type WEB for your server (needed to verify user ID and offline access)
   //   offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
   //   hostedDomain: '', // specifies a hosted domain restriction
   //   forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
